70845441d4a5caef96cb0d20dd38fe520ddf743a
David Mann <david.mann@optimizely.com>
2018-10-29 11:42:47 -0500

    Add script to switch to LTS Node environment via NVM.

---
+++ env-setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# if [[ "$(uname)" != "Darwin" ]]
+# then
+#   echo "This script only works on MacOS!"
+#   exit 1
+# fi
+
+test "$(uname)" == "Darwin" || { echo >&2 "Install script only compatible with MacOS."; exit 1; }
+type brew > /dev/null 2>&1 || { echo >&2 "Install script prerequisite: Homebrew must be installed."; exit 1; }
+brew ls --versions nvm > /dev/null || { echo >&2 "Install script prerequisite: NVM must be installed via Homebrew."; exit 1; }
+
+# ----
+
+# Make "nvm" builtin available to rest of script
+source $(brew --prefix nvm)/nvm.sh
+
+nvm use 2> /dev/null
+
+if [[ $? -eq 0 ]]
+then
+  echo "Using existing LTS node release."
+else
+  echo "Installing latest LTS node release ..."
+  nvm install && nvm use
+fi
