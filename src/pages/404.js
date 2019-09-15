541f0237ea2ed7f2d5f4f57d3268406564773796
Jen Downs <hello@jendowns.com>
2019-09-15 15:21:23 -0500

    chore: update custom error page, update dependencies

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
 
 const NotFoundPage = () => (
   <Layout>
     <main id="404" className="container">
       <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>
+        You just hit a route that doesn&#39;t exist!{" "}
+        <span role="img" aria-label="ghost">
+          👻
+        </span>
+      </p>
     </main>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
