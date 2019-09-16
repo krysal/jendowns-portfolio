4f31b4bb4febffbcf2c04ed0e000f4beab145b4d
Jen Downs <hello@jendowns.com>
2019-09-15 20:40:00 -0500

    fix: run prettier, update svg attributes

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,10 +1,10 @@
-import React from "react";
-import Helmet from "react-helmet";
-import { Link, StaticQuery, graphql } from "gatsby";
-import moment from "moment";
-import Layout from "../components/Layout";
+import React from 'react'
+import Helmet from 'react-helmet'
+import { Link, StaticQuery, graphql } from 'gatsby'
+import moment from 'moment'
+import Layout from '../components/Layout'
 
-import { externalPosts } from "../../data/externalPosts";
+import { externalPosts } from '../../data/externalPosts'
 
 export default () => {
   const renderPosts = data => {
@@ -14,21 +14,21 @@ export default () => {
         title: post.node.childMarkdownRemark.frontmatter.title,
         path: post.node.childMarkdownRemark.frontmatter.path,
         publication: undefined,
-        date: post.node.childMarkdownRemark.frontmatter.date
-      };
-      allPosts.push(normalizedPost);
+        date: post.node.childMarkdownRemark.frontmatter.date,
+      }
+      allPosts.push(normalizedPost)
     })
 
     allPosts = allPosts.concat(externalPosts)
 
     allPosts.forEach(post => {
-      post.sortOrder = moment(post.date, "DD MMM YYYY").valueOf()
+      post.sortOrder = moment(post.date, 'DD MMM YYYY').valueOf()
     })
 
     // Sort in descending order:
     allPosts.sort((a, b) => {
-      return b.sortOrder - a.sortOrder;
-    });
+      return b.sortOrder - a.sortOrder
+    })
 
     return allPosts.map(post => {
       const renderLink = () => {
@@ -44,7 +44,7 @@ export default () => {
               <div className="container--posts__box--right" />
               <div className="container--posts__box--bottom" />
             </a>
-          );
+          )
         }
 
         return (
@@ -58,8 +58,8 @@ export default () => {
             <div className="container--posts__box--right" />
             <div className="container--posts__box--bottom" />
           </Link>
-        );
-      };
+        )
+      }
 
       return (
         <article
@@ -69,9 +69,9 @@ export default () => {
         >
           {renderLink()}
         </article>
-      );
-    });
-  };
+      )
+    })
+  }
 
   return (
     <StaticQuery
@@ -101,11 +101,19 @@ export default () => {
         <Layout>
           <main className="container container--posts">
             <Helmet title={`Jen Downs - Posts`} />
-            <h1>Things I've written <span role="img" aria-label="woman">💁🏼‍♀️</span><span role="img" aria-label="pencil and paper">📝</span></h1>
+            <h1>
+              Things I've written...{' '}
+              <span role="img" aria-label="woman">
+                💁🏼‍♀️
+              </span>
+              <span role="img" aria-label="pencil and paper">
+                📝
+              </span>
+            </h1>
             {renderPosts(data)}
           </main>
         </Layout>
       )}
     />
-  );
-};
+  )
+}
