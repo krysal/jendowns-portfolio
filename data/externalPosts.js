5417fd66d4af625f02b1a0e8c4b381b261ef8c3e
Jen Downs <hello@jendowns.com>
2019-09-15 20:13:31 -0500

    feat: combine posts and sort, add new external post, update grid styling

--- data/externalPosts.js
+++ data/externalPosts.js
@@ -1,13 +1,19 @@
 module.exports = {
   externalPosts: [
     {
-      title: "Custom WordPress Sidebar",
+      title: "Carbon for IBM Security",
+      path: "https://medium.com/carbondesign/carbon-for-ibm-security-986ccfe5c012",
+      publication: "Carbon Design System",
+      date: "20 Aug 2019",
+    },
+    {
+      title: "Creating a Sidebar of Categories and Posts in WordPress",
       path: "https://medium.com/we-are-mammoth/creating-a-sidebar-of-categories-and-posts-in-wordpress-8e5f45e3f4f8",
-      publication: null,
+      publication: "We Are Mammoth",
       date: "20 Oct 2016",
     },
     {
-      title: "JavaScript Arcade Game in Unity 5",
+      title: "Creating a 2D Arcade Game with JavaScript in Unity 5",
       path: "https://recompilermag.com/issues/issue-3/creating-a-2d-arcade-game-with-javascript-in-unity-5/",
       publication: "The Recompiler",
       date: "01 Apr 2016",
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,50 +1,77 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { Link, StaticQuery, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from "react";
+import Helmet from "react-helmet";
+import { Link, StaticQuery, graphql } from "gatsby";
+import moment from "moment";
+import Layout from "../components/Layout";
 
-import { externalPosts } from '../../data/externalPosts'
+import { externalPosts } from "../../data/externalPosts";
 
 export default () => {
-  const renderPosts = data =>
-    data.allFile.edges.map(post => (
-      <article
-        aria-label={post.node.childMarkdownRemark.frontmatter.title}
-        className="container--posts__box"
-        key={post.node.childMarkdownRemark.frontmatter.path}
-      >
-        <Link to={post.node.childMarkdownRemark.frontmatter.path}>
-          <h3>
-            <span>{post.node.childMarkdownRemark.frontmatter.title}</span>
-          </h3>
-          <h4>
-            <span>{post.node.childMarkdownRemark.frontmatter.date}</span>
-          </h4>
-          <div className="container--posts__box--right" />
-          <div className="container--posts__box--bottom" />
-        </Link>
-      </article>
-    ))
+  const renderPosts = data => {
+    let allPosts = []
+    data.allFile.edges.forEach(post => {
+      const normalizedPost = {
+        title: post.node.childMarkdownRemark.frontmatter.title,
+        path: post.node.childMarkdownRemark.frontmatter.path,
+        publication: undefined,
+        date: post.node.childMarkdownRemark.frontmatter.date
+      };
+      allPosts.push(normalizedPost);
+    })
 
-  const renderPublishedPosts = externalPosts =>
-    externalPosts.map(post => (
-      <article
-        aria-label={post.title}
-        className="container--posts__box"
-        key={post.path}
-      >
-        <a href={post.path} target="_blank" rel="noopener noreferrer">
-          <h3>
-            <span>{post.title}</span>
-          </h3>
-          <h4>
-            <span>{post.date}</span>
-          </h4>
-          <div className="container--posts__box--right" />
-          <div className="container--posts__box--bottom" />
-        </a>
-      </article>
-    ))
+    allPosts = allPosts.concat(externalPosts)
+
+    allPosts.forEach(post => {
+      post.sortOrder = moment(post.date, "DD MMM YYYY").valueOf()
+    })
+
+    // Sort in descending order:
+    allPosts.sort((a, b) => {
+      return b.sortOrder - a.sortOrder;
+    });
+
+    return allPosts.map(post => {
+      const renderLink = () => {
+        if (post.publication) {
+          return (
+            <a href={post.path} target="_blank" rel="noopener noreferrer">
+              <h3>
+                <span>{post.title}</span>
+              </h3>
+              <h4>
+                <span>{post.date}</span>
+              </h4>
+              <div className="container--posts__box--right" />
+              <div className="container--posts__box--bottom" />
+            </a>
+          );
+        }
+
+        return (
+          <Link to={post.path}>
+            <h3>
+              <span>{post.title}</span>
+            </h3>
+            <h4>
+              <span>{post.date}</span>
+            </h4>
+            <div className="container--posts__box--right" />
+            <div className="container--posts__box--bottom" />
+          </Link>
+        );
+      };
+
+      return (
+        <article
+          aria-label={post.title}
+          className="container--posts__box"
+          key={post.path}
+        >
+          {renderLink()}
+        </article>
+      );
+    });
+  };
 
   return (
     <StaticQuery
@@ -74,11 +101,11 @@ export default () => {
         <Layout>
           <main className="container container--posts">
             <Helmet title={`Jen Downs - Posts`} />
+            <h1>Things I've written <span role="img" aria-label="woman">💁🏼‍♀️</span><span role="img" aria-label="pencil and paper">📝</span></h1>
             {renderPosts(data)}
-            {renderPublishedPosts(externalPosts)}
           </main>
         </Layout>
       )}
     />
-  )
-}
+  );
+};
