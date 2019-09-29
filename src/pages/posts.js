aa3e53f2c79fc63088e2cb1faecf010c9f2b0a67
Jen Downs <hello@jendowns.com>
2019-09-29 00:51:38 -0500

    fix: change how articles on posts page look, update link transitions

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,77 +1,64 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { Link, StaticQuery, graphql } from 'gatsby'
-import moment from 'moment'
-import Layout from '../components/Layout'
+import React from "react";
+import Helmet from "react-helmet";
+import { Link, StaticQuery, graphql } from "gatsby";
+import moment from "moment";
+import Layout from "../components/Layout";
 
-import { externalPosts } from '../../data/externalPosts'
+import { externalPosts } from "../../data/externalPosts";
 
 export default () => {
   const renderPosts = data => {
-    let allPosts = []
+    let allPosts = [];
     data.allFile.edges.forEach(post => {
       const normalizedPost = {
         title: post.node.childMarkdownRemark.frontmatter.title,
         path: post.node.childMarkdownRemark.frontmatter.path,
         publication: undefined,
-        date: post.node.childMarkdownRemark.frontmatter.date,
-      }
-      allPosts.push(normalizedPost)
-    })
+        date: post.node.childMarkdownRemark.frontmatter.date
+      };
+      allPosts.push(normalizedPost);
+    });
 
-    allPosts = allPosts.concat(externalPosts)
+    allPosts = allPosts.concat(externalPosts);
 
     allPosts.forEach(post => {
-      post.sortOrder = moment(post.date, 'DD MMM YYYY').valueOf()
-    })
+      post.sortOrder = moment(post.date, "DD MMM YYYY").valueOf();
+    });
 
     // Sort in descending order:
     allPosts.sort((a, b) => {
-      return b.sortOrder - a.sortOrder
-    })
+      return b.sortOrder - a.sortOrder;
+    });
 
     return allPosts.map(post => {
       const renderLink = () => {
         if (post.publication) {
           return (
-            <a href={post.path} target="_blank" rel="noopener noreferrer">
-              <h3>
-                <span>{post.title}</span>
-              </h3>
-              <h4>
-                <span>{post.date}</span>
-              </h4>
-              <div className="container--posts__box--right" />
-              <div className="container--posts__box--bottom" />
-            </a>
-          )
+            <>
+              <a href={post.path} target="_blank" rel="noopener noreferrer">
+                {post.title}
+              </a>
+              <small className="publication">{post.publication}</small>
+              <small className="date">{post.date}</small>
+            </>
+          );
         }
 
         return (
-          <Link to={post.path}>
-            <h3>
-              <span>{post.title}</span>
-            </h3>
-            <h4>
-              <span>{post.date}</span>
-            </h4>
-            <div className="container--posts__box--right" />
-            <div className="container--posts__box--bottom" />
-          </Link>
-        )
-      }
+          <>
+            <Link to={post.path}>{post.title}</Link>
+            <small className="date">{post.date}</small>
+          </>
+        );
+      };
 
       return (
-        <article
-          aria-label={post.title}
-          className="container--posts__box"
-          key={post.path}
-        >
+        <article aria-label={post.title} className="post" key={post.path}>
           {renderLink()}
         </article>
-      )
-    })
-  }
+      );
+    });
+  };
 
   return (
     <StaticQuery
@@ -101,19 +88,11 @@ export default () => {
         <Layout>
           <main className="container container--posts">
             <Helmet title={`Jen Downs - Posts`} />
-            <h1>
-              Things I've written...{' '}
-              <span role="img" aria-label="woman">
-                💁🏼‍♀️
-              </span>
-              <span role="img" aria-label="pencil and paper">
-                📝
-              </span>
-            </h1>
+            <h1>things I've written</h1>
             {renderPosts(data)}
           </main>
         </Layout>
       )}
     />
-  )
-}
+  );
+};
